d6efbbcc49652ece0d4beae3f89f8fe710c92c74
Jan Warchoł <jan.warchol@gmail.com>
2020-04-27 14:16:29 +0200

    Highlighting important parts of output

--- key-signing.sh
+++ key-signing.sh
@@ -1,7 +1,7 @@
 ./gpg-user-setup.sh Alice
 ./gpg-user-setup.sh Bob
 
-# helpers for showing commands (on stderr to avoid mixing with actual output)
+# helpers for displaying commands (on stderr to avoid mixing with actual output)
 alice_does() {
   echo -e "\e[1;37m> Alice runs:  $@ \e[0m" >&2
   eval "GNUPGHOME=Alice $@"
@@ -13,42 +13,60 @@ bob_does() {
   echo "" >&2
 }
 
+# helpers for visually marking important parts of output
+highlight() {
+  sed -E "s/($@)/[1;33m\1[0m/g"
+}
+hi_fingerprint() {
+  sed -E "s/([0-9A-F ]{16,})/[1;33m\1[0m/g"
+}
+
+# Bob writes a message to Alice and he wants to encrypt it
+bob_does echo "Hi Alice, Bob here." > msg-to-alice
+
+# He gets Alice's public key
 alice_does \
 gpg --armor --export alice@example.com > alice.pub
-ll alice.pub
-
 bob_does \
 gpg --import alice.pub
 
-# Note that Alice's key is marked "unknown"
+# Note that Alice's key is marked "unknown" (it's not verified)
 bob_does \
-gpg --list-keys
+gpg --list-keys | highlight "unknown"
 
-# encrypting will show a warning because Alice's key needs to be verified
-echo msg from Bob to Alice | GNUPGHOME=Bob gpg -a -e -r alice@example.com | tee bob-to-alice.asc
-
-# The only signature is Alice's own, but we don't
+# Because of that trying to encrypt with it will show a warning
 bob_does \
-gpg --check-signatures alice@example.com
+gpg --armor --encrypt --recipient alice@example.com msg-to-alice
 
-# Alice tells her key fingerprint to Bob
+# Alice's key is self-signed - that signature doesn't prove anything
+bob_does \
+gpg --check-signatures alice@example.com | highlight "sig.*Alice"
+
+# Alice tells her key fingerprint to Bob (in person or in other really secure way)
 alice_does \
-gpg --fingerprint alice@example.com
+gpg --fingerprint alice@example.com | hi_fingerprint
 
-# Bob signs the key verifying fingerprint with what Alice said
+# Bob verifies the fingerprint and, if it matches, signs the key with his own key
+# (confirming this key really belongs to Alice)
 bob_does \
 gpg --sign-key alice@example.com
 
-# Now encryption should work
-echo msg from Bob to Alice | GNUPGHOME=Bob gpg -a -e -r alice@example.com | tee bob-to-alice.asc
+# Now Alice's key is marked as verified
+bob_does \
+gpg --list-keys alice@example.com | highlight "full"
+
+# And encryption will work without warning
+bob_does \
+gpg -a -e -r alice@example.com msg-to-alice
+cat msg-to-alice.asc; echo
 
 # And Alice is able to decrypt the message
 alice_does \
-gpg -d bob-to-alice.asc
+gpg -d msg-to-alice.asc
 
 # Now Alice's key is signed by Bob
 bob_does \
-gpg --check-signatures alice@example.com
+gpg --check-signatures alice@example.com | highlight "bob@example.com"
 
 # Bob exports Alice's key with his signature and gives it to Alice
 bob_does \
@@ -60,7 +78,7 @@ gpg --import alice-signed.pub
 
 # Alice has Bob's signature, but doesn't have his key to check it
 alice_does \
-gpg --check-signatures alice@example.com
+gpg --check-signatures alice@example.com | highlight "signature not checked"
 
 bob_does \
 gpg --armor --export bob@example.com > bob.pub
@@ -70,4 +88,4 @@ gpg --import bob.pub
 
 # Now Alice can see her key is verified by Bob
 alice_does \
-gpg --check-signatures alice@example.com
+gpg --check-signatures alice@example.com | highlight "bob@example.com"
