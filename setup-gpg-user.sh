22cada0a48e15538b73266a5b34347b63b94276d
Jan Warchoł <jan.warchol@gmail.com>
2020-05-21 16:27:33 +0200

    Fix support for spaces in user name

--- setup-gpg-user.sh
+++ setup-gpg-user.sh
@@ -12,19 +12,19 @@ KEY_EXPIRY="${2:-1w}"
 BASE_DIR="$(dirname $(readlink --canonicalize "$0"))"
 GNUPGHOME="$BASE_DIR/$USER_NAME"
 LOG_FILE="$GNUPGHOME/tutorial-setup.log"
-mkdir -p -m 700 $GNUPGHOME
+mkdir -p -m 700 "$GNUPGHOME"
 export GNUPGHOME
-echo GPG data belonging to $USER_NAME will be stored in $GNUPGHOME.
+echo -e "GPG data belonging to $USER_NAME will be stored in \e[1;37m$GNUPGHOME\e[0m".
 
 # Only run actual key generation if it wasn't done before
 if [ -e "$LOG_FILE" ]; then
   echo "GPG home for $USER_NAME already exists, nothing to do."; echo; exit;
 fi
 
-echo; echo Generating new key for $USER_NAME:
+echo; echo "Generating new key for $USER_NAME <$USER_EMAIL>:"
 # Log to a file for later reference (e.g. to get ID after script finished).
 # Note: keys are short because performance > security for testing purposes
-gpg --batch --generate-key --logger-file $LOG_FILE <<EOF
+gpg --batch --generate-key --logger-file "$LOG_FILE" <<EOF
   Key-Type: default
   Key-Length: 1024
   Subkey-Type: default
@@ -34,15 +34,16 @@ gpg --batch --generate-key --logger-file $LOG_FILE <<EOF
   Passphrase: $USER_PASS
   Expire-Date: $KEY_EXPIRY
 EOF
-cat $LOG_FILE
+cat "$LOG_FILE"
 echo
 
-KEY_ID=$(tail -1 $LOG_FILE | grep -Eo "[0-9A-F]{40}")
+KEY_ID=$(tail -1 "$LOG_FILE" | grep -Eo "[0-9A-F]{40}")
 
 # Trigger database maintenance (could be confusing if it appears on its own)
 gpg --check-trustdb; echo
 
 # Cache some information for other scripts
-echo -e "Generated key ID: \e[1;37m$KEY_ID\e[0m"
+echo -e "Generated key with ID \e[1;37m$KEY_ID\e[0m"
+echo -e "and passphrase \e[1;37m$USER_PASS\e[0m."
 echo Writing down key ID to $GNUPGHOME/key-id.
-echo $KEY_ID > $GNUPGHOME/key-id
+echo $KEY_ID > "$GNUPGHOME/key-id"
