9aa1a4c357687ec2ddf0412055243665a00923a2
Jan Warchoł <jan.warchol@gmail.com>
2025-01-31 20:29:25 +0100

    WIP

--- 0.1-viewing-keys.sh
+++ 0.1-viewing-keys.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-source utils.sh
-
-# This tutorial was tested with GPG 2.2.
+# This tutorial was tested with GPG 2.2.19
 # Check your version; you may have to use `gpg2` command to get GPG v2
-log_and_run  gpg --version  | highlight "gpg.*[1-9]\.[0-9]\..*"
+gpg --version | grep "gpg.*[1-9]\.[0-9]\..*"; echo
 
+source utils.sh
 # Create test identity
 ./setup-test-gpg-user.sh Alice
 export GNUPGHOME=Alice
+
 
 # View all known keys (yours and other people). Short option: -k
 log_and_run  gpg --list-keys  | highlight "^pub|sub"
 # View all available secret keys (~= your keys). Short option: -K
 log_and_run  gpg --list-secret-keys  | highlight "^sec|ssb"
 
-# Legend:
+# Explanation:
 # pub = primary public key
 # sub = subkey
 # uid = user identity (many identities can be tied to one key)
@@ -36,7 +36,7 @@ log_and_run  gpg -k --list-options show-unusable-subkeys
 log_and_run  gpg -k --fingerprint
 
 # Show keygrips, which are used for private key filenames:
-log_and_run  gpg -K --with-keygrip
+log_and_run  gpg -K --with-keygrip  | highlight "Keygrip"
 log_and_run  ls -l $GNUPGHOME/private-keys-v1.d/
 
 # To see more details, including trust value, use --edit-key dialog:
--- 0.2-encrypting-data.sh
+++ 0.2-encrypting-data.sh
@@ -1,33 +1,50 @@
 #!/bin/bash
 
 source utils.sh
-
 # create test identity
-./setup-test-gpg-user.sh Ala
-export GNUPGHOME=Ala
+./setup-test-gpg-user.sh Alice
+export GNUPGHOME=Alice
 
 
 # Create a file with sample data.
-log_and_run  echo "My secret data" > my_data
+echo "My secret data" > example
+# Remove files from previous runs.
+rm -f example.gpg example.asc example_signed.asc
+
+
+# Encryption
+
+# When encrypting, you must specify the recipient (who should be able to
+# read the data). You can use name, email or key fingerprint for that.
 
 # Encrypt a file using binary format (short options: -e -r)
-# Note that recipient name must follow -r.
-# Note that the original file remains on disk.
-log_and_run  gpg --encrypt --recipient Ala my_data
-log_and_run  ls -l my_data.gpg
+log_and_run  gpg --encrypt --recipient Alice example
+log_and_run  file example.gpg
 
 # Encrypt in an ASCII-armored format (short option: -a)
-log_and_run  gpg --armor -er Ala my_data
-log_and_run  cat my_data.asc
+log_and_run  gpg --armor -e -r Alice example
+log_and_run  file example.asc
+log_and_run  cat example.asc
 
 # Encrypt and sign the data (short option: -s)
-# Note that all options must be specified before input file
-log_and_run  gpg --sign -aer Ala --output data_signed.asc my_data 
-log_and_run  cat data_signed.asc
-# notice that the file is longer than the one without signature
+log_and_run  gpg --sign -a -e -r Alice --output example_signed.asc example
+log_and_run  cat example_signed.asc
+
+# Notes:
+# - the original file remains on disk
+# - all options must be specified before input file name
+# - signed file is longer than the one without signature
 
 
-# Decrypt data:
-log_and_run gpg -d my_data.gpg
-log_and_run gpg -d my_data.asc
-log_and_run gpg -d data_signed.asc |& highlight "signature"
+# Decryption
+
+# Decrypt data (gpg will prompt for key passphrase):
+log_and_run  gpg -d example.gpg
+log_and_run  gpg -d example.asc
+
+# Note: ID mentioned here is the ID of the *encryption subkey*, not main key.
+log_and_run  gpg -d example.asc  |& highlight "[0-9A-F]{16,}"
+log_and_run  gpg -k --keyid-format long  |& highlight "\/[0-9A-F]{16}"
+
+# GPG displays signature automatically if present.
+log_and_run  gpg -d example_signed.asc  |& highlight "signature"
